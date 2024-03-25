1885d6da9a6b6493b169411dcb27d5aa3ecfb819
Sakura Koffron <147252329+sakunyann@users.noreply.github.com>
2024-03-24 20:31:14 -0700

    Update pickle.py

--- pickle.py
+++ pickle.py
@@ -1,6 +1,5 @@
-import pickle
-
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.linear_model import LinearRegression
